1d974995ae7ac8628b98f8e57888efc8d532b313
EC2 Default User <ec2-user@ip-172-31-0-188.ec2.internal>
2022-04-18 20:48:44 +0000

    adding "http://" in front of all of URI to fix CORS block

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Build the client from react source code
 REACT_APP_API_URL=$(ec2-metadata --public-hostname)':8080'
-REACT_APP_API_URL=$(echo $REACT_APP_API_URL | sed 's/public-hostname: //')
+REACT_APP_API_URL='http://'$(echo $REACT_APP_API_URL | sed 's/public-hostname: //')
 REACT_APP_AUTH_URL=$(ec2-metadata --public-hostname)':8081'
-REACT_APP_AUTH_URL=$(echo $REACT_APP_AUTH_URL | sed 's/public-hostname: //')
+REACT_APP_AUTH_URL='http://'$(echo $REACT_APP_AUTH_URL | sed 's/public-hostname: //')
 echo 'REACT_APP_API_URL="'$REACT_APP_API_URL'"' > /home/ec2-user/todoapp/client/.env
 echo 'REACT_APP_AUTH_URL="'$REACT_APP_AUTH_URL'"' >> /home/ec2-user/todoapp/client/.env
 cd /home/ec2-user/todoapp/client
